bed0a3e784da646bc285350456ad23acb5df3ecc
Mike Gordon <vprmatrix55@gmail.com>
2022-01-03 20:33:10 -0700

    add issue list to second page

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,3 +1,5 @@
+var issueContainerEl = document.querySelector("#issues-container");
+
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
@@ -5,7 +7,7 @@ var getRepoIssues = function(repo) {
         // request was successful
         if (response.ok) {
           response.json().then(function(data) {
-            console.log(data);
+            displayIssues(data);
           });
         }
         else {
@@ -13,4 +15,38 @@ var getRepoIssues = function(repo) {
         }
     });  
 }
-  getRepoIssues("facebook/react");
+
+var displayIssues = function(issues) {
+
+  if (issues.length === 0) {
+    issueContainerEl.textContent = "This repo has no open issues!";
+    return;
+  }
+
+  for (var i=0; i < issues.length; i++) {
+    var issueEl = document.createElement("a");
+    issueEl.classList = "list-item flex-row justify-space-between align-center";
+    issueEl.setAttribute("href", issues[i].html_url);
+    issueEl.setAttribute("target", "_blank");
+
+    var titleEl = document.createElement("span");
+    titleEl.textContent = issues[i].title;
+
+    issueEl.appendChild(titleEl);
+
+    var typeEl = document.createElement("span");
+
+    if (issues[i].pull_request) {
+      typeEl.textContent = "(Pull request)";
+
+    } else {
+      typeEl.textContent = "(Issue)";
+    }
+
+    issueEl.appendChild(typeEl);
+    issueContainerEl.appendChild(issueEl);
+  }
+};
+
+
+  getRepoIssues("mike2481/robot-gladiators");
